b5af51fdf3b34a56def25399c0f385474b8cf95e
sarafotros <sarafotros@gmail.com>
2020-05-19 15:03:35 +0100

    squres

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score');
+    const StartButton = document.querySelector('#start-button');
+    const width = 10;
+    
+
+
+
+});
